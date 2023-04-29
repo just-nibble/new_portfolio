9b5566b8239b965daae0d8af0e0c43eaddaf97de
Princewill Olaiya <princewillolaiya@gmail.com>
2023-04-30 00:24:47 +0100

    added about me

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(
+    [
+        models.About, models.CV, models.Niche,
+        models.Profile, models.Social, models.Testimonial,
+        models.Title
+    ]
+)
--- core/views.py
+++ core/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
+from . import models 
 # Create your views here.
 
 
 
 def homepage(request):
-    return render(request, 'index.html')
+    context = {
+        'about': models.About.objects.get(id=1)
+    }
+    return render(request, 'index.html', context=context)
 
 def porfolio_page(request):
     return render(request, 'portfolio-1.html')
