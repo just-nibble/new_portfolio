ec3e46505afe83be023ad72720f98509b4aaacec
Princewill Olaiya <princewillolaiya@gmail.com>
2023-05-05 19:21:29 +0100

    env secret key

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -14,23 +14,23 @@ import os
 
 from pathlib import Path
 
+environ.Env.read_env()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jz#ajxqmdpnw18(!&dh%g)pn#iklomb1t!d2h2gk1s@&9y%1h)'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, True)
 )
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-environ.Env.read_env()
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = env('DEBUG')
 
